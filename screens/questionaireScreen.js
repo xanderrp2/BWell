c1632de06952bc0ae5d9a70cba884e0783064e57
Xander Rutherford-Parish <Xanderdrakerp@gmail.com>
2022-08-05 16:51:19 -0400

    Create questionaireScreen.js

---
+++ screens/questionaireScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Journal({ navigation }) {
+    <View style={styles.container}>
+      <Text style={styles.container}>This is the journal screen</Text>
+    </View>
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
